b582c18666eba1f0b2e1c2c4ba22de8c5b855674
Hamsath Nejeeb <hamsathnajeeb@gmail.com>
2026-01-04 16:18:05 +0000

    now when using vercel to publish the website the voting part is not show

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,6 @@
 export const firebaseConfig = {
-  "projectId": "studio-5142264276-280d0",
-  "appId": "1:928880870634:web:533216b58815787bc64393",
-  "apiKey": "AIzaSyDLUkG-Snm6222iG8KiqnyTh55DO2cVwhs",
-  "authDomain": "studio-5142264276-280d0.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "928880870634"
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
 };
